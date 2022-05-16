e2cfaa1f298750ad7f063c98621fd4b16faed1c3
Nicolas SANS <nicolas.sansd@gmail.com>
2022-05-16 23:02:55 +0200

    initial commit

---
+++ src/main/java/fr/nikho/manhunt/Manhunt.java
@@ -0,0 +1,16 @@
+package fr.nikho.manhunt;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Manhunt extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
